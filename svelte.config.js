7c1ba198218d5a0a4765e6bf5df1ca45c1e0e064
dagulv <dag@webbmaffian.se>
2025-04-21 13:56:03 +0200

    Added github workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,15 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import { mdsvex } from "mdsvex";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-	// preprocess: [
-	// 	vitePreprocess(),
-	// 	mdsvex({
-	// 		extensions: [".md"]
-	// 	}),
-	// ],
-	// extensions: [".svelte", ".md"],
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
-			precompress: false,
+			precompress: true,
 			strict: true
 		}),
 		alias: {
